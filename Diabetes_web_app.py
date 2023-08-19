88e36f2664dfea7e1369f386a258a66da9c5422f
Tanishi Nayak <100431225+TanishiNayak@users.noreply.github.com>
2023-08-19 20:19:14 +0530

    Add files via upload

--- Diabetes_web_app.py
+++ Diabetes_web_app.py
@@ -9,7 +9,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('C:/IGDTUW/Machine Learning project/trained_model.sav', 'rb'))
 
 #creating a func for prediction
 
--- predictivesystem.py
+++ predictivesystem.py
@@ -8,7 +8,7 @@ This is a temporary script file.
 import numpy as np
 import pickle
 
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('C:/IGDTUW\Machine Learning project/trained_model.sav', 'rb'))
 
 input_data = (5,166,72,19,175,25.8,0.587,51)
 
